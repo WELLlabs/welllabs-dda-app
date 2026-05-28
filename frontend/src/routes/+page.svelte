a03012485f5f1ebda75f5c50088ee4df266f2c8f
Devipriya <devipriya@meyicloud.com>
2026-05-28 16:25:25 +0530

    changes in script

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -56,7 +56,7 @@
     try {
 
       const response = await fetch(
-        `http://127.0.0.1:8000/api/watershed/?lat=${selectedLat}&lng=${selectedLng}`
+        `/api/watershed/?lat=${selectedLat}&lng=${selectedLng}`
       );
 
       watershed = await response.json();
