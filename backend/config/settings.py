6a614cb13d005e8bf50db1fe8087f90ffea5c0be
Devipriya <devipriya@meyicloud.com>
2026-05-29 12:34:48 +0530

    changes apllication file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,12 +44,9 @@ environ.Env.read_env(
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 # Application definition
 
